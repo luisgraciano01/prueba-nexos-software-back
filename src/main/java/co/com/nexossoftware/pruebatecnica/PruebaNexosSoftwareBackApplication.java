a449fec86fbad74577a5d60d608da4e6218c978d
Luis Graciano <luiiixxxofficial@gmail.com>
2021-08-07 11:55:58 -0500

    initial spring boot project

---
+++ src/main/java/co/com/nexossoftware/pruebatecnica/PruebaNexosSoftwareBackApplication.java
@@ -0,0 +1,13 @@
+package co.com.nexossoftware.pruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaNexosSoftwareBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaNexosSoftwareBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/nexossoftware/pruebatecnica/PruebaNexosSoftwareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.nexossoftware.pruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaNexosSoftwareBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
